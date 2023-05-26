47ec01412b042b01e933e13ebaff3cc66c54b24d
louistiti <louis.grenard@gmail.com>
2023-05-26 10:53:25 +0800

    feat(card): add no border option

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,4 +1,5 @@
 import type React from 'react'
+import classNames from 'classnames'
 
 import styles from './card.module.sass'
 
@@ -6,11 +7,24 @@ interface Props {
   children: React.ReactNode
   width?: string | number
   height?: string | number
+  hasBorder?: boolean
 }
 
-export default function Card({ children, width = 'auto', height = 'auto' }: Props) {
+export default function Card({
+  children,
+  width = 'auto',
+  height = 'auto',
+  hasBorder = true
+}: Props) {
   return (
-    <div className={`aurora-card ${styles.cardContainer}`} style={{ width, height }}>
+    <div
+      className={classNames({
+        'aurora-card': true,
+        [styles.cardContainer]: true,
+        [styles.noBorder]: !hasBorder
+      })}
+      style={{ width, height }}
+    >
       <div className={styles.body}>
         {children}
       </div>
