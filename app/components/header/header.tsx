08ef0bc9da5e224814c9f8fb77fb482f182fe12d
louistiti <louis.grenard@gmail.com>
2023-05-31 23:15:32 +0800

    feat(components): add anchor and back home icons

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import type React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import styles from './header.module.sass'
 
@@ -8,6 +12,9 @@ interface Props {
 }
 
 export default function Header({ title, description }: Props) {
+  const pathname = usePathname()
+  const isHome = pathname === '/'
+
   return (
     <>
       <svg className={styles.bgGrid} width="971" viewBox="0 0 971 200" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -21,8 +28,17 @@ export default function Header({ title, description }: Props) {
         </defs>
       </svg>
       <header>
-        <h1 className={styles.title}>{title}</h1>
-        {description && <p>{description}</p>}
+        {!isHome && (
+          <Link href="/" style={{ position: 'absolute' }}>
+            <svg className={styles.backIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16">
+              <path d="M4.83594 12.0001L11.043 18.2072L12.4573 16.793L7.66436 12.0001L12.4573 7.20718L11.043 5.79297L4.83594 12.0001ZM10.4858 12.0001L16.6929 18.2072L18.1072 16.793L13.3143 12.0001L18.1072 7.20718L16.6929 5.79297L10.4858 12.0001Z" />
+            </svg>
+          </Link>
+        )}
+        <div>
+          <h1 className={styles.title}>{title}</h1>
+          {description && <p>{description}</p>}
+        </div>
       </header>
     </>
   )
