9402c01241cb031aca226d21e79215bb2d93130b
louistiti <louis.grenard@gmail.com>
2023-06-12 21:58:03 +0800

    feat: add icon to forecast widget example

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -43,22 +43,6 @@ export default function Component() {
             <Icon name="home-heart" size="xl" bgColor="blue" />
             <Icon name="home-heart" size="xl" bgShape="circle" />
             <Icon name="home-heart" size="md" bgColor="secondary-yellow" color="yellow" />
-            <Icon size="xl" svg={
-              <svg viewBox="0 0 134 132" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" clipRule="evenodd" d="M72.5478 69.1152L80.4523 74.3849L66.3755 95.5001H90.5398L64.5174 131.531L56.8159 125.969L71.9601 105H48.6245L72.5478 69.1152Z" fill="url(#paint0_linear_81_184)"/>
-                <path fillRule="evenodd" clipRule="evenodd" d="M38.5 103.409C16.6613 97.684 0.5 77.174 0.5 52.75C0.5 23.8931 23.0601 0.5 50.8894 0.5C73.9775 0.5 93.4389 16.6014 99.3999 38.5659C100.073 38.5222 100.751 38.5 101.434 38.5C119.144 38.5 133.5 53.3865 133.5 71.7499C133.5 90.1133 119.144 105 101.434 105C101.038 105 100.643 104.992 100.25 104.978V95.5H101.689V95.4983C113.721 95.3553 124 85.1001 124 71.7499C124 58.3056 113.575 48 101.434 48C98.0042 48 94.7111 48.8226 91.7514 50.306C91.6005 47.5424 91.1952 44.8495 90.5594 42.2521L90.5451 42.2579C86.0435 23.5377 69.7734 10 50.8894 10C28.6285 10 10 28.8122 10 52.75C10 72.0623 22.1249 88.0384 38.5 93.4915V103.409Z" fill="url(#paint1_linear_81_184)"/>
-                <defs>
-                <linearGradient id="paint0_linear_81_184" x1="69.5822" y1="69.1152" x2="69.5822" y2="131.531" gradientUnits="userSpaceOnUse">
-                <stop stopColor="#F5BD52"/>
-                <stop offset="1" stopColor="#F5DA79"/>
-                </linearGradient>
-                <linearGradient id="paint1_linear_81_184" x1="67" y1="0.5" x2="67" y2="105" gradientUnits="userSpaceOnUse">
-                <stop stopColor="#95B6F6"/>
-                <stop offset="1" stopColor="#5193DE"/>
-                </linearGradient>
-                </defs>
-              </svg>
-            } />
           </Flexbox>
         </Section>
         <Section title="Image">
@@ -115,9 +99,29 @@ export default function Component() {
             </Flexbox>
           </WidgetWrapper>
           <WidgetWrapper>
-            <Flexbox alignItems="center" justifyContent="center" flexDirection="column" rowGap="xs">
-              <Text fontSize="lg" fontWeight="semi-bold">Paris</Text>
-              <Text secondary>Thursday, 1 June</Text>
+            <Flexbox flexDirection="column" rowGap="xl">
+              <Flexbox alignItems="center" justifyContent="center" flexDirection="column" rowGap="xs">
+                <Text fontSize="lg" fontWeight="semi-bold">Paris</Text>
+                <Text secondary>Thursday, 1 June</Text>
+              </Flexbox>
+              <Flexbox alignItems="center" justifyContent="center">
+                <Icon size="xl" svg={
+                  <svg viewBox="0 0 134 132" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M72.5478 69.1152L80.4523 74.3849L66.3755 95.5001H90.5398L64.5174 131.531L56.8159 125.969L71.9601 105H48.6245L72.5478 69.1152Z" fill="url(#paint0_linear_81_184)"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M38.5 103.409C16.6613 97.684 0.5 77.174 0.5 52.75C0.5 23.8931 23.0601 0.5 50.8894 0.5C73.9775 0.5 93.4389 16.6014 99.3999 38.5659C100.073 38.5222 100.751 38.5 101.434 38.5C119.144 38.5 133.5 53.3865 133.5 71.7499C133.5 90.1133 119.144 105 101.434 105C101.038 105 100.643 104.992 100.25 104.978V95.5H101.689V95.4983C113.721 95.3553 124 85.1001 124 71.7499C124 58.3056 113.575 48 101.434 48C98.0042 48 94.7111 48.8226 91.7514 50.306C91.6005 47.5424 91.1952 44.8495 90.5594 42.2521L90.5451 42.2579C86.0435 23.5377 69.7734 10 50.8894 10C28.6285 10 10 28.8122 10 52.75C10 72.0623 22.1249 88.0384 38.5 93.4915V103.409Z" fill="url(#paint1_linear_81_184)"/>
+                    <defs>
+                    <linearGradient id="paint0_linear_81_184" x1="69.5822" y1="69.1152" x2="69.5822" y2="131.531" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#F5BD52"/>
+                    <stop offset="1" stopColor="#F5DA79"/>
+                    </linearGradient>
+                    <linearGradient id="paint1_linear_81_184" x1="67" y1="0.5" x2="67" y2="105" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#95B6F6"/>
+                    <stop offset="1" stopColor="#5193DE"/>
+                    </linearGradient>
+                    </defs>
+                  </svg>
+                } />
+              </Flexbox>
             </Flexbox>
           </WidgetWrapper>
         </Section>
